4f60bfa41d0f6e431ce725b9ec01b800337a5667
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-23 16:21:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+	if speed <= 40
+		puts true
+	elsif speed >= 60
+		puts true
+	else
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
+	speed < 40 ? true : false
 	
+
 end
-	
-
-
